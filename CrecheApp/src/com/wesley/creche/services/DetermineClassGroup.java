31a3b12c4ad99c92e52867cf9a74ae4ae471eb29
wesleydewet <wesleydewet@gmail.com>
2014-10-31 21:43:54 +0000

    Registration form updated, child service updated, data inserting successfully into database

---
+++ CrecheApp/src/com/wesley/creche/services/DetermineClassGroup.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wesley.creche.services;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author Wesleyjohn
+ */
+public class DetermineClassGroup {
+    
+    public String getGrade(Date dob){
+        String grade = null;
+        
+        GregorianCalendar c = new GregorianCalendar();
+        
+        
+        return grade;
+    }
+}
