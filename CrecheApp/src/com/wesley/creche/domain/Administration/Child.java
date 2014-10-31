eb3d5bd015581fbd5d31a82b20f765caf2878a2b
wesleydewet@gmail.com <wesleydewet@gmail.com@54dd9bea-86a7-6a09-43fb-025b755d3f29>
2014-10-31 15:25:56 +0000

    Registration form created, data sent through service to backend

--- CrecheApp/src/com/wesley/creche/domain/Administration/Child.java
+++ CrecheApp/src/com/wesley/creche/domain/Administration/Child.java
@@ -5,59 +5,19 @@
  */
 package com.wesley.creche.domain.Administration;
 
-import com.wesley.creche.domain.EmployeeService.Employees.Teacher;
-import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Temporal;
 
 /**
  *
  * @author Wesleyjohn
  */
-@Entity
-public class Child implements Serializable {
-    private String ID;
+public class Child {
     private String name;
-    @Temporal(javax.persistence.TemporalType.DATE)
-    private Date dob;
-    private char gender;
-    private char race;
-    
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Teacher> teacher;
-    
-    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
-    private Parent parent;
+    private String lastName;
+    private String IDNumber;
 
     public Child() {
-    }
-
-    public Child(String ID, String name, Date dob, char gender, char race, List<Teacher> teacher, Parent parent, Medical medical, Long id) {
-        this.ID = ID;
-        this.name = name;
-        this.dob = dob;
-        this.gender = gender;
-        this.race = race;
-        this.teacher = teacher;
-        this.parent = parent;
-        this.medical = medical;
-        this.id = id;
-    }
-
-    public String getID() {
-        return ID;
-    }
-
-    public void setID(String ID) {
-        this.ID = ID;
+        
     }
 
     public String getName() {
@@ -68,6 +28,38 @@ public class Child implements Serializable {
         this.name = name;
     }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIDNumber() {
+        return IDNumber;
+    }
+
+    public void setIDNumber(String IDNumber) {
+        this.IDNumber = IDNumber;
+    }
+
+    public String getChildID() {
+        return childID;
+    }
+
+    public void setChildID(String childID) {
+        this.childID = childID;
+    }
+
+    public String getMedicalConditions() {
+        return medicalConditions;
+    }
+
+    public void setMedicalConditions(String medicalConditions) {
+        this.medicalConditions = medicalConditions;
+    }
+
     public Date getDob() {
         return dob;
     }
@@ -75,87 +67,16 @@ public class Child implements Serializable {
     public void setDob(Date dob) {
         this.dob = dob;
     }
+    private String childID;
+    private String medicalConditions;
+    private Date dob;
 
-    public char getGender() {
-        return gender;
+    public Child(String name, String lastName, String IDNumber, String childID, String medicalConditions, Date dob) {
+        this.name = name;
+        this.lastName = lastName;
+        this.IDNumber = IDNumber;
+        this.childID = childID;
+        this.medicalConditions = medicalConditions;
+        this.dob = dob;
     }
-
-    public void setGender(char gender) {
-        this.gender = gender;
-    }
-
-    public char getRace() {
-        return race;
-    }
-
-    public void setRace(char race) {
-        this.race = race;
-    }
-
-    public List<Teacher> getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(List<Teacher> teacher) {
-        this.teacher = teacher;
-    }
-
-    public Parent getParent() {
-        return parent;
-    }
-
-    public void setParent(Parent parent) {
-        this.parent = parent;
-    }
-
-    public Medical getMedical() {
-        return medical;
-    }
-
-    public void setMedical(Medical medical) {
-        this.medical = medical;
-    }
-    
-    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
-    private Medical medical;
-    
-    
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Child)) {
-            return false;
-        }
-        Child other = (Child) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "com.wesley.creche.domain.Child[ id=" + id + " ]";
-    }
-    
 }
---
+++ CrecheApp/src/com/wesley/creche/services/ChildService.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wesley.creche.services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+/**
+ *
+ * @author Wesleyjohn
+ */
+public class ChildService {
+    
+    public void insertChildData(String name, String lastName, String id1, String medical, Date d) throws ClassNotFoundException, SQLException{
+        
+        String driverName = "com.mysql.jdbc.Driver";
+        Class.forName(driverName);
+
+        String serverName = "localhost";
+        String mydatabase = "creche";
+        String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+
+        String username = "root";
+        String password = "";
+
+        System.out.println("about to send to database"+name+lastName);
+        
+        Connection connection = DriverManager.getConnection(url, username, password);
+        Statement stmt = connection.createStatement();
+        
+        int id = Integer.parseInt(id1);
+        
+        stmt.executeUpdate("INSERT INTO child"
+                +"(id_num)"
+                +"values("+id+")");
+        stmt.close();
+        
+        System.out.println("sent to database");
+    }
+}
