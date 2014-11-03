442f9568b6ee9e0e4fc294be793c278bd487a6e1
wesleydewet@gmail.com <wesleydewet@gmail.com@54dd9bea-86a7-6a09-43fb-025b755d3f29>
2014-11-03 20:58:48 +0000

    Administration form added, adding/ editing staff/ child forms added, functionality to be done

--- CrecheApp/src/com/wesley/creche/services/AdministrationServices/ChildService.java
+++ CrecheApp/src/com/wesley/creche/services/AdministrationServices/ChildService.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.wesley.creche.services;
+package com.wesley.creche.services.AdministrationServices;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
