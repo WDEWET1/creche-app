2a91f9ab6c42255c06f8db8386b681fef003650c
taswell21@gmail.com <taswell21@gmail.com@54dd9bea-86a7-6a09-43fb-025b755d3f29>
2014-11-08 20:47:02 +0000

    Added a table to finance form with some test data and 2 radio buttons

--- CrecheApp/src/com/wesley/creche/client/desktop/Finance/Finance.java
+++ CrecheApp/src/com/wesley/creche/client/desktop/Finance/Finance.java
@@ -22,13 +22,14 @@ public class Finance extends javax.swing.JFrame {
     Styles style = new Styles();
     
     public Finance() {
-        super("Accounting");
+        super("Finance");
         initComponents();
         
         style.setFrameStyles(this);
         style.setButtonStyle(jButton1);
-        //setLocationRelativeTo(null);
-        //getContentPane().setBackground(Color.white);
+        buttonGroup1.add(jRadioButton1);
+        buttonGroup1.add(jRadioButton2);                
+        
     }
 
     /**
@@ -40,7 +41,12 @@ public class Finance extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonGroup1 = new javax.swing.ButtonGroup();
         jButton1 = new javax.swing.JButton();
+        jRadioButton1 = new javax.swing.JRadioButton();
+        jRadioButton2 = new javax.swing.JRadioButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -51,21 +57,65 @@ public class Finance extends javax.swing.JFrame {
             }
         });
 
+        jRadioButton1.setText("Income");
+        jRadioButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton1ActionPerformed(evt);
+            }
+        });
+
+        jRadioButton2.setText("Expenditure");
+        jRadioButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton2ActionPerformed(evt);
+            }
+        });
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jButton1)
-                .addContainerGap(317, Short.MAX_VALUE))
+                .addGap(17, 17, 17)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 636, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jButton1)))
+                        .addGap(33, 33, 33))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jRadioButton1)
+                        .addGap(26, 26, 26)
+                        .addComponent(jRadioButton2)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(19, 19, 19)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jRadioButton1)
+                    .addComponent(jRadioButton2))
+                .addGap(43, 43, 43)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 232, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 85, Short.MAX_VALUE)
                 .addComponent(jButton1)
-                .addContainerGap(258, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
@@ -78,9 +128,56 @@ public class Finance extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1ActionPerformed
+        // TODO add your handling code here:
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {"Tazz", 1234, 2222, 333},
+                {"Waz", 1234, 2222, 333},
+                {"Clifford", 1111, 3333, 123345},
+                {"ady", 111, 222, 123},
+                {"Waz", 1234, 2222, 333},
+                {"Clifford", 1111, 3333, 123345},
+                {"ady", 111, 222, 123},
+                {"Waz", 1234, 2222, 333},
+                {"Clifford", 1111, 3333, 123345},
+                {"ady", 111, 222, 123},
+                {"Waz", 1234, 2222, 333},
+                {"Clifford", 1111, 3333, 123345},
+                {"ady", 111, 222, 123},
+                {"Waz", 1234, 2222, 333},
+                {"Clifford", 1111, 3333, 123345},
+                {"ady", 111, 222, 123}
+            },
+            new String [] {
+                "Name", "Number 1", "Number 2", "Number 3"
+            }
+        ));
+    }//GEN-LAST:event_jRadioButton1ActionPerformed
+
+    private void jRadioButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton2ActionPerformed
+        // TODO add your handling code here:
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {"Tazz", 1234, 2222, 333,1111},
+                {"Was", 1234, 2222, 333,1111},
+                {"Clifford", 1111, 3333, 123345,1111},
+                {"ady", 111, 222, 123,1111}
+            },
+            new String [] {
+                "Name", "Number 1", "Number 2", "Number 3", "Number 4"
+            }
+        ));
+    }//GEN-LAST:event_jRadioButton2ActionPerformed
+
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JButton jButton1;
+    private javax.swing.JRadioButton jRadioButton1;
+    private javax.swing.JRadioButton jRadioButton2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
