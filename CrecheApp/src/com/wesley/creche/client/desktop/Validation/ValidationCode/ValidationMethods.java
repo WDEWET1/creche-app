399f0f0dca44de1a40d60db26e9676775b4160b9
taswell21@gmail.com <taswell21@gmail.com>
2014-11-08 21:20:35 +0000

    Added a validation class please add your validations here and call the method thanks

---
+++ CrecheApp/src/com/wesley/creche/client/desktop/Validation/ValidationCode/ValidationMethods.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.wesley.creche.client.desktop.Validation.ValidationCode;
+
+/**
+ *
+ * @author TaswellSalie
+ */
+public class ValidationMethods {
+    
+    //Add validation code/Methods here
+    
+    public void validateIfInteger(String text){
+        
+    }
+    
+    public void ValidateIfStrings(String text){
+        
+    }
+}
