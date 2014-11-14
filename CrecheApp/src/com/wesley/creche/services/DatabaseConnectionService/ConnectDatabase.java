b2d1fad27b87341c790a1c9554483a2e8a753322
taswell21@gmail.com <taswell21@gmail.com>
2014-11-14 19:32:47 +0000

    database connection made to singleton call this method if you want to commit to the DB no need to open and close connection every time.

---
+++ CrecheApp/src/com/wesley/creche/services/DatabaseConnectionService/ConnectDatabase.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.wesley.creche.services.DatabaseConnectionService;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author TaswellSalie
+ */
+public class ConnectDatabase {
+       Connection connect  = null;
+        
+        public Connection getConnection(Connection connection) throws ClassNotFoundException, SQLException {
+            if(connect == null){
+                System.out.println("in if");
+                return createConnection(connection);
+            }
+            else{
+                System.out.println("in else");
+                return connect;
+            }
+        
+        }
+        
+        public Connection createConnection(Connection con) throws ClassNotFoundException, SQLException{
+        
+        String driverName = "com.mysql.jdbc.Driver";
+        Class.forName(driverName);
+        String serverName = "localhost";
+        String mydatabase = "creche";
+        String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+        String username = "root";
+        String password = "";   
+        
+        //Connection connection = DriverManager.getConnection(url, username, password);
+           con = DriverManager.getConnection(url, username, password);
+        return con;    
+        }
+}
