9d933f152cd1d215a4ceba0406048bbc829aa661
taswell21@gmail.com <taswell21@gmail.com@54dd9bea-86a7-6a09-43fb-025b755d3f29>
2014-11-16 02:28:09 +0000

    Added UpdateFeesService test case set to fail

---
+++ CrecheApp/test/com/wesley/creche/services/FinancialServices/UpdateFeesServiceTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.wesley.creche.services.FinancialServices;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author TaswellSalie
+ */
+public class UpdateFeesServiceTest {
+    
+    public UpdateFeesServiceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of updateChildFees method, of class UpdateFeesService.
+     */
+    @Test
+    public void testUpdateChildFees() throws Exception {
+        System.out.println("updateChildFees");
+        double updatedAmount = 0.0;
+        int fees_id = 0;
+        UpdateFeesService.updateChildFees(updatedAmount, fees_id);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
