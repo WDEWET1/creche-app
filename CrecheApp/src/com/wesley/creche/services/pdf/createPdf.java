529ead554268ff341c1e85272e0637299b2b2295
karstens.clive <karstens.clive@gmail.com>
2014-11-16 20:39:13 +0000

    Finalized Financial Reports

--- CrecheApp/src/com/wesley/creche/services/pdf/createPdf.java
+++ CrecheApp/src/com/wesley/creche/services/pdf/createPdf.java
@@ -9,6 +9,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.draw.DottedLineSeparator;
 
 import com.wesley.creche.services.createFolder.createFolder;
 import com.wesley.creche.services.SQLQueries;
@@ -22,11 +23,58 @@ import java.util.Date;
  * @author KarstensC
  */
 public class createPdf {
+      private static Font catFont = new Font(Font.FontFamily.TIMES_ROMAN, 18,
+      Font.BOLD);
+      private static Font redFont = new Font(Font.FontFamily.TIMES_ROMAN, 12,
+          Font.NORMAL, BaseColor.RED);
+      private static Font subFont = new Font(Font.FontFamily.TIMES_ROMAN, 16,
+          Font.BOLD);
+      private static Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12,
+      Font.BOLD);
+      private static Font italic = new Font(Font.FontFamily.TIMES_ROMAN, 14,
+      Font.ITALIC);
+      private static Font courier = new Font(Font.FontFamily.COURIER, 12,
+      Font.ITALIC);
     
     private static void createFolderIfNotExist() {
         createFolder createFolder = new createFolder();
         createFolder.createFolder();
     }
+    
+    private static void addEmptyLine(Paragraph paragraph, int number) {
+        for (int i = 0; i < number; i++) {
+          paragraph.add(new Paragraph(" "));
+        }
+    }
+    
+    private static void addEmptyLine(Document document, int number) throws DocumentException {
+         Paragraph paragraph = new Paragraph();
+        for (int i = 0; i < number; i++) {
+          paragraph.add(new Paragraph(" "));
+          document.add(paragraph);
+        }
+    }
+    
+    private static void addLine(Document document) throws DocumentException {
+        Paragraph paragraph = new Paragraph();
+        paragraph.add(new Paragraph("-----------------------------------------------------------------------------------"));
+        document.add(paragraph);
+    }
+    
+    private static void addTitle(Document document)
+      throws DocumentException {
+        Paragraph preface = new Paragraph();
+        // We add one empty line
+        addEmptyLine(preface, 1);
+        // Lets write a big header
+        preface.add(new Paragraph("Financial Statement", catFont));
+
+        addEmptyLine(preface, 1);
+        
+        preface.add(new Paragraph("Report generated on: " + new Date(), smallBold));
+        
+        document.add(preface);
+  }
     
     public void writePdfFinancialReport(String name, String startDate,
             String accStatus, String amountDue) throws DocumentException, IOException {
@@ -53,11 +101,29 @@ public class createPdf {
         
         document.open();
         
-        Paragraph par = new Paragraph("Financial Report : " + today);
-        par.getFont().setStyle(Font.BOLD);
-        document.add(par);
+        addTitle(document);
+        addLine(document);
         
-        //document.add(new paragraph("-----------------------------"));
+        document.add(new Paragraph("Name : " + name + " " + lastName, italic));
+        addEmptyLine(document, 2);
+        
+        Paragraph paragraph = new Paragraph();
+        paragraph.add("Start Date :");
+        paragraph.setTabSettings(new TabSettings());
+        paragraph.add(Chunk.TABBING);
+        paragraph.add(new Chunk(startDate));
+        document.add(paragraph);
+        addEmptyLine(document, 1);
+        
+        document.add(new Paragraph("Account Status : " + accStatus));
+        addEmptyLine(document, 1);
+        
+        document.add(new Paragraph("Total Amount Outstanding : R" + amountDue));
+        
+        
+        
+        
+        
         
         
         
